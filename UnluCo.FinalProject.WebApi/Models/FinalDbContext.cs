a77e15597a1a52187c6e0e791923ba45c555c3d1
kodluyoruz <baristutakli@hotmail.com>
2022-02-16 19:00:36 +0300

    Registered entities

--- UnluCo.FinalProject.WebApi/Models/FinalDbContext.cs
+++ UnluCo.FinalProject.WebApi/Models/FinalDbContext.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 
 namespace UnluCo.FinalProject.WebApi.Models
 {
-    public class FinalDbContext: IdentityDbContext<User>
+    public class FinalDbContext : IdentityDbContext<User>
     {
-        public FinalDbContext(DbContextOptions<FinalDbContext> dbcontext):base(dbcontext)
+        public FinalDbContext(DbContextOptions<FinalDbContext> dbcontext) : base(dbcontext)
         {
 
         }
-        
-        DbSet<Product> Products { get; set; }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Offer> Offers { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
 
     }
 }
