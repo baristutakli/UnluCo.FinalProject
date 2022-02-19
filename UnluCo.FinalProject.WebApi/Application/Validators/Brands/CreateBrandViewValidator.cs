bb6666c3ebb03a7b19c970d3a45931679ea1f8ea
kodluyoruz <baristutakli@hotmail.com>
2022-02-19 20:21:08 +0300

    BrandViewValidators

---
+++ UnluCo.FinalProject.WebApi/Application/Validators/Brands/CreateBrandViewValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCo.FinalProject.WebApi.Application.ViewModels.BrandsViewModel;
+
+namespace UnluCo.FinalProject.WebApi.Application.Validators.Brands
+{
+    public class CreateBrandViewValidator:AbstractValidator<CreateBrandViewModel>
+    {
+        public CreateBrandViewValidator()
+        {
+            RuleFor(b => b.Name).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ UnluCo.FinalProject.WebApi/Application/Validators/Brands/DeleteBrandViewValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCo.FinalProject.WebApi.Application.ViewModels.BrandsViewModel;
+
+namespace UnluCo.FinalProject.WebApi.Application.Validators.Brands
+{
+    public class DeleteBrandViewValidator:AbstractValidator<DeleteBrandViewModel>
+    {
+        public DeleteBrandViewValidator()
+        {
+            RuleFor(b => b.Id).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
