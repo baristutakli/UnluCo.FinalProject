d4e45f9301a756ebb2287d47fa4b6fbcc4f27db6
kodluyoruz <baristutakli@hotmail.com>
2022-02-20 08:44:42 +0300

    ProductViewModelValidators

---
+++ UnluCo.FinalProject.WebApi/Application/Validators/Products/UpdateProductViewModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCo.FinalProject.WebApi.Application.ViewModels.ProductsViewModel;
+
+namespace UnluCo.FinalProject.WebApi.Application.Validators.Products
+{
+    public class UpdateProductViewModelValidator : AbstractValidator<UpdateProductViewModel>
+    {
+        public UpdateProductViewModelValidator()
+        {
+            RuleFor(vm => vm.Id).GreaterThan(0).NotEmpty().NotNull();
+            RuleFor(vm => vm.IsOfferable).NotNull();
+            RuleFor(vm => vm.Name).MaximumLength(100).NotNull().NotEmpty();
+            RuleFor(vm => vm.Description).MaximumLength(500).NotEmpty().NotNull();
+            RuleFor(vm => vm.Price).GreaterThan(0).NotNull().NotEmpty();
+            RuleFor(vm => vm.Color).NotNull();
+            RuleFor(vm => vm.Brand).NotNull();
+            RuleFor(vm => vm.Category).NotNull();
+            RuleFor(vm => vm.User).NotNull();
+        }
+    }
+}
