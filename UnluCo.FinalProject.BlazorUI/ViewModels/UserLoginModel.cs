c002584a0cccc41a6b4b632a54fd17fe32b92cf3
kodluyoruz <baristutakli@hotmail.com>
2022-02-24 11:07:29 +0300

    Added new pages

---
+++ UnluCo.FinalProject.BlazorUI/ViewModels/UserLoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace UnluCo.FinalProject.BlazorUI
+{
+    public class UserLoginModel : ComponentBase
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8),MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
