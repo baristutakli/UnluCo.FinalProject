2a16ba08a251b411735869bfb7c6b6e883d39db7
kodluyoruz <baristutakli@hotmail.com>
2022-02-24 11:46:33 +0300

    RegisterUserPage

---
+++ UnluCo.FinalProject.BlazorUI/ViewModels/RegisterUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace UnluCo.FinalProject.BlazorUI
+
+{
+    public class RegisterUserModel : ComponentBase
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8), MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ UnluCo.FinalProject.BlazorUI/ViewModels/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnluCo.FinalProject.BlazorUI
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
