9513e549b401f00dc6d8a02ff840eb75306d8352
kodluyoruz <baristutakli@hotmail.com>
2022-02-25 10:57:41 +0300

    Created createProductPage

---
+++ UnluCo.FinalProject.BlazorUI/ViewModels/CategoryTitleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCo.FinalProject.WebApi.Application.ViewModels.ProductsViewModel;
+
+namespace UnluCo.FinalProject.BlazorUI
+{
+    public class CategoryTitleViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ UnluCo.FinalProject.BlazorUI/ViewModels/ColorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnluCo.FinalProject.BlazorUI
+{
+    public class ColorViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
